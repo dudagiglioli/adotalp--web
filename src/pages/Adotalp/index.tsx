7c6568f359989c3936424568b911ad4ffc5ba866
dudagiglioli <dudaguilice@gmail.com>
2023-08-21 12:29:01 -0300

    Img adopdet

--- src/pages/Adotalp/index.tsx
+++ src/pages/Adotalp/index.tsx
@@ -3,8 +3,12 @@ import logo from "../../assets/logo.png";
 
 export default function Adotalp(){
     return(
+        
    <div className={styles.toolbar}>
+
        <img src= {logo} alt="Logo AdotaLP" className={styles.logo}/>
+       <h1 className={styles.title}>ADOTA LP</h1>
+
     </div>
     )
 }
