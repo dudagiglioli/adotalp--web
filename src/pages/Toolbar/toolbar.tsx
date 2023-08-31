02ffc4a7012cd3ce4336785b854f4e7558c80992
dudagiglioli <dudaguilice@gmail.com>
2023-08-31 12:16:38 -0300

    toolbar responsive :heavy_check_mark:

--- src/pages/Toolbar/toolbar.tsx
+++ src/pages/Toolbar/toolbar.tsx
@@ -2,12 +2,20 @@ import styles from "./Toolbar.module.scss";
 import logo from "../../assets/logo.png";
 
 export default function Toolbar() {
-  return (
-    <header className={styles.toolbar}>
-     
-        <img src={logo} alt="Logo AdotaLP" className={styles.logo} />
-        <h1>lksdhgkjsh</h1>
-     
-    </header>
-  );
+ return(
+   <header className={styles.toolbar}>
+
+     <div className={styles.start}>
+      <img src={logo} alt="Logo Adota LP" className={styles.logo}/>
+     </div>
+
+     <div className={styles.middle}>
+       teste
+     </div>
+
+     <div className={styles.end}>
+       dlsd
+     </div>
+   </header>
+ )
 }
