db8ff964842ae355631b5be163034f6fe93fadc2
MelloNickolas <nickolasphmello10@gmail.com>
2023-09-20 15:10:03 -0300

    Content and firstContent

--- src/pages/Cadastro/cadastro.tsx
+++ src/pages/Cadastro/cadastro.tsx
@@ -18,7 +18,7 @@ export default function Cadastro(){
                     <h2 className={styles.secondColumn__title}> crie sua conta </h2>
 
                     <div className={styles.secondColumn__socialMedia}>
-                        <ul>
+                        <ul className={styles.secondColumn__socialMedia__list}>
                             <li>  facebook </li>
                             <li>  google+  </li>
                             <li>  linkedin </li>
@@ -40,15 +40,15 @@ export default function Cadastro(){
 
         <div className={styles.secondContent}>
             <div className={styles.firstColumn}>
-                    <h2 className={styles.firstColumn__title}> Obrigado por Voltar!</h2>
-                    <p className={styles.firstColumn__description}> Continue coom a gente</p>
-                    <p className={styles.firstColumn__description}> logue com sua conta pessoal</p>
-                    <button className={styles.firstColumn__btn}> login </button>
+                    <h2 className={styles.firstColumn__title}> Bem-Vindo! </h2>
+                    <p className={styles.firstColumn__description}> Crie sua conta pessoal </p>
+                    <p className={styles.firstColumn__description}> e adote seu pet com a gente</p>
+                    <button className={styles.firstColumn__btn}> cadastro </button>
             </div>
                 {/* first column */}
 
                 <div className={styles.secondColumn}>
-                    <h2 className={styles.secondColumn__title}> crie sua conta </h2>
+                    <h2 className={styles.secondColumn__title}> Entre com sua conta </h2>
 
                     <div className={styles.secondColumn__socialMedia}>
                         <ul>
@@ -58,13 +58,13 @@ export default function Cadastro(){
                         </ul>
                     </div>
 
-                    <p className={styles.secondColumn__description}> ou use seu e-mail para se registrar: </p>
+                    <p className={styles.secondColumn__description}> ou use seu e-mail para entrar: </p>
 
                     <form className={styles.form}> 
-                        <input type="text" placeholder="Nome"/>
                         <input type="email" placeholder="Email"/>
                         <input type="password" placeholder="Senha"/>
-                        <button className={styles.form__btn}> cadastro </button>
+                        <button>esquceu sua senha?</button>
+                        <button className={styles.form__btn}> login </button>
                     </form>
                 </div>
              </div>
@@ -76,4 +76,6 @@ export default function Cadastro(){
 
 }
 
-// https://www.youtube.com/watch?v=GtsNZtzZiec&list=PLARhMvcrqcLy3bU1RxbkC7GYH7eHgXI0A 19:43
+// https://www.youtube.com/watch?v=NbMb0QPr2Iw&list=PLARhMvcrqcLy3bU1RxbkC7GYH7eHgXI0A&index=2 13:30
+
+// colocar content e first content
