82ef3380c0717d15c07f0fc8e033dd21828b4e20
dudagiglioli <dudaguilice@gmail.com>
2023-10-03 10:45:46 -0300

    validations :heavy_check_mark:

--- src/pages/Cadastro/cadastro.tsx
+++ src/pages/Cadastro/cadastro.tsx
@@ -38,36 +38,6 @@ const handleSubmit = (values: any) => {
   console.log(values);
 };
 
-function mascaraTelefone(event) {
-  let tecla = event.key;
-  let telefone = event.target.value.replace(/\D+/g, "");
-
-  if (/^[0-9]$/i.test(tecla)) {
-      telefone = telefone + tecla;
-      let tamanho = telefone.length;
-
-      if (tamanho >= 12) {
-          return false;
-      }
-      
-      if (tamanho > 10) {
-          telefone = telefone.replace(/^(\d\d)(\d{5})(\d{4}).*/, "($1) $2-$3");
-      } else if (tamanho > 5) {
-          telefone = telefone.replace(/^(\d\d)(\d{4})(\d{0,4}).*/, "($1) $2-$3");
-      } else if (tamanho > 2) {
-          telefone = telefone.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
-      } else {
-          telefone = telefone.replace(/^(\d*)/, "($1");
-      }
-
-      event.target.value = telefone;
-  }
-
-  if (!["Backspace", "Delete"].includes(tecla)) {
-      return false;
-  }
-}
-
 export default function Cadastro() {
   return (
     <>
@@ -166,8 +136,8 @@ export default function Cadastro() {
                   </label>
                   <input
                     type="tel" 
-                    onkeydown="return mascaraTelefone(event)"
                     placeholder="(xx) xxxxx-xxxx"
+                    pattern="([0-9]{2})[0-9]{5}-[0-9]{4} "
                     required
                     className={styles.inputBox__input}
                   ></input>
